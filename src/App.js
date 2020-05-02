17b9aa937c859b36c992eed2390750ad9156beb9
Edson Perotoni <perotoni@gmail.com>
2020-05-01 23:02:44 -0300

    Adiciona funcionalidades Listar e Curtir repositorios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 import {
   SafeAreaView,
   View,
@@ -11,8 +11,29 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
+    console.log(id);
+    const repository = await api.post("/repositories/" + id +'/like').then((response)=>{
+      const repository = response.data;
+      console.log(repository);
+      const repositoriesClone = [...repositories];
+      const repositorieIndex = repositoriesClone.findIndex(
+        (repository) => repository.id === id
+      );
+      repositoriesClone[repositorieIndex].likes = repository.likes;
+      setRepositories(repositoriesClone);
+    });
+    
+    
   }
 
   return (
@@ -20,35 +41,44 @@ export default function App() {
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
         <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+          <FlatList
+            data={repositories}
+            keyExtractor={(repository) => repository.id}
+            renderItem={({ item: repository }) => (
+              <>
+                <Text key={repository.id} style={styles.repository}>
+                  {repository.title}
+                </Text>
+                <View style={styles.techsContainer}>
+                  {repository.techs.map((tech, index) => (
+                    <Text key={index} style={styles.tech}>
+                      {tech}
+                    </Text>
+                  ))}
+                </View>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
+                <View style={styles.likesContainer}>
+                  <>
+                    <TouchableOpacity
+                      style={styles.button}
+                      onPress={() => handleLikeRepository(repository.id)}
+                      // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                      testID={`like-button-${repository.id}`}
+                    >
+                      <Text style={styles.buttonText}>Curtir</Text>
+                    </TouchableOpacity>
+                    <Text
+                      style={styles.likeText}
+                      // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                      testID={`repository-likes-${repository.id}`}
+                    >
+                      {repository.likes} curtidas
+                    </Text>
+                  </>
+                </View>
+              </>
+            )}
+          />
         </View>
       </SafeAreaView>
     </>
